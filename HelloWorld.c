c62ceb62339d255b2c20a1889f95756bb57aa3db
zahmnur <zxa161@miami.edu>
2022-11-06 19:09:26 -0500

    My first git commit

--- HelloWorld.c
+++ HelloWorld.c
@@ -6,10 +6,10 @@
 //-----------------------------------------------------------------------------
 int main(void) {
 
-    printf("Welcome to my world\n");
+    //printf("Welcome to my world\n");
     printf("Hello world\n");
-    printf("Have a nice day\n");
-    printf("Have a most excellent day\n");
+    //printf(Have a nice day\n");
+    //printf("Have a most excellent day\n");
 
     return(EXIT_SUCCESS);
 }
